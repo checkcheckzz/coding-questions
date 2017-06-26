bb5db0c8edd8d2b19e30b8dcced8dba0375ea89b
Tony Campbell <evilegg@yahoo.com>
2017-06-26 14:00:48 -0400

    Add counter to HasAcquaintance()

--- problem/Array/CelebrityProblem.py
+++ problem/Array/CelebrityProblem.py
@@ -14,20 +14,42 @@ HasAcquaintance as possible.
 """
 
 
-def HasAcquaintance(A, B):
-    """d is the celebrity."""
-    edges = {
-        'a': ('b', 'c', 'd'),
-        'b': ('c', 'd'),
-        'c': ('a', 'd'),
-        'd': (),
-        'e': ('b', 'd'),
-    }
-    return B in edges.get(A, ())
+D_IS_THE_CELEBRITY = {
+    'a': ('b', 'c', 'd'),
+    'b': ('c', 'd'),
+    'c': ('a', 'd'),
+    'd': (),
+    'e': ('b', 'd'),
+}
+
+
+class Solution:
+    def __init__(self, edges=None):
+        self.edges = edges or {}
+        self.has_acquaintance_counter = 0
+
+    def HasAcquaintance(self, A, B):
+        """Returns True if A knows B."""
+        self.has_acquaintance_counter += 1
+        return B in self.edges.get(A, ())
+
+    def __call__(self, people=None):
+        if people is None:
+            people = self.edges.keys()
+        celebrity = self.findTheCelebrity(people)
+        return self.has_acquaintance_counter, celebrity
+
+    def findTheCelebrity(self, people):
+        """Return the name of the celebrity if they are at the party.
+
+        Return None if there is no celebrity at the party.
+        """
+        return None
 
 
 def main():
-    pass
+    s = Solution(D_IS_THE_CELEBRITY)
+    print s()
 
 
 if __name__ == '__main__':
